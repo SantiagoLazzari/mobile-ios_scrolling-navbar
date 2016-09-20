d151b6d578ceaba28e0000d3f63b7a6b7e343a43
SantiagoLazzari <santiago.lazzari@mercadolibre.com>
2016-09-20 17:22:55 -0300

    Auto scrolling enable

--- MLScrollingNavbar/Classes/MLSNScrollingHandler/MLSNScrollingHandler.m
+++ MLScrollingNavbar/Classes/MLSNScrollingHandler/MLSNScrollingHandler.m
@@ -13,6 +13,8 @@
 @property (nonatomic) NSLayoutConstraint *scrollViewTopConstraint;
 @property (nonatomic) UIView *navbar;
 @property (nonatomic) UIScrollView *scrollView;
+
+// El offset del contentOffset pasado
 @property (nonatomic) CGFloat previousOffset;
 
 // El límite de cuánto tiene que scrollear el navbar
@@ -23,6 +25,8 @@
 
 // La diferencia de scrolling que tuvo
 @property (nonatomic) CGFloat deltaScrolling;
+
+@property (nonatomic) NSUInteger draggingCount;
 
 @end
 
@@ -35,9 +39,11 @@
     scrollingHandler.navbar = navbar;
     scrollingHandler.scrollView = scrollView;
     scrollingHandler.currentScrolling = - [[UIApplication sharedApplication] statusBarFrame].size.height;
+    scrollingHandler.draggingCount = 0;
     
     [scrollView addObserver:scrollingHandler forKeyPath:@"contentOffset" options:NSKeyValueObservingOptionNew context:nil];
-    
+    [scrollView addObserver:scrollingHandler forKeyPath:@"pan.state" options:NSKeyValueObservingOptionNew context:nil];
+
     return scrollingHandler;
 }
 
@@ -46,7 +52,6 @@
     return self.navbar.frame.size.height + [[UIApplication sharedApplication] statusBarFrame].size.height;
 }
 
-
 - (CGFloat)deltaScrolling {
     CGFloat delta = self.scrollView.contentOffset.y - self.previousOffset;
     
@@ -54,11 +59,19 @@
 }
 
 #pragma mark - Observing
-- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
-{
-    if ([keyPath isEqualToString:@"contentOffset"])
-    {
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    if ([keyPath isEqualToString:@"contentOffset"]) {
         [self scrollNavbar];
+    }
+    
+    if ([keyPath isEqualToString:@"pan.state"]) {
+        self.draggingCount += (self.scrollView.dragging) ? 1 : -1;
+        
+        if (self.draggingCount == 1) {
+            self.draggingCount = 0;
+        
+            [self autoScroll];
+        }
     }
 }
 
@@ -120,14 +133,33 @@
     CGFloat navbarHeight = CGRectGetHeight(self.navbar.frame);
     
     self.navbar.frame = CGRectMake(navbarX, navbarY, navbarWidth, navbarHeight);
-
 }
 
 - (void)collapseScrollViewTopConstraint {
     
 }
 
+#pragma mark - Autoscroll
+- (BOOL)mightCollide {
+    return self.currentScrolling + [[UIApplication sharedApplication] statusBarFrame].size.height >= self.scrollingLimit / 2.0;
+}
 
+- (BOOL)mightExpand {
+    return self.currentScrolling + [[UIApplication sharedApplication] statusBarFrame].size.height < self.scrollingLimit / 2.0;
+}
+
+- (void)autoScroll {
+    if ([self mightCollide]) {
+        CGPoint finalPoint = CGPointMake(self.scrollView.contentOffset.x, self.scrollView.contentOffset.y + self.scrollingLimit - self.currentScrolling);
+        
+        [self.scrollView setContentOffset:finalPoint animated:YES];
+    } else {
+        CGPoint finalPoint = CGPointMake(self.scrollView.contentOffset.x, self.scrollView.contentOffset.y - (self.currentScrolling + [[UIApplication sharedApplication] statusBarFrame].size.height));
+        
+        [self.scrollView setContentOffset:finalPoint animated:YES];
+        
+    }
+}
 
 #pragma mark - Memory
 - (void)dealloc {
