18ae309fbfbdf8467af811c6a9b84a5860b598f4
SantiagoLazzari <santiago.lazzari@mercadolibre.com>
2016-09-20 11:01:42 -0300

    Initial commit

---
+++ Example/MLScrollingNavbar/MLSNViewController.m
@@ -0,0 +1,29 @@
+//
+//  MLSNViewController.m
+//  MLScrollingNavbar
+//
+//  Created by SantiagoLazzari on 09/20/2016.
+//  Copyright (c) 2016 SantiagoLazzari. All rights reserved.
+//
+
+#import "MLSNViewController.h"
+
+@interface MLSNViewController ()
+
+@end
+
+@implementation MLSNViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
